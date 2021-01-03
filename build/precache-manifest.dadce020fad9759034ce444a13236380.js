3008074d79a4dc094372ff3b7ef1897ea2895143
Rudra <43309139+RudraPramanik@users.noreply.github.com>
2021-01-04 01:30:59 +0600

    Add files via upload

---
+++ build/precache-manifest.dadce020fad9759034ce444a13236380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf156c3a2a95d6f8ee653128d57fb33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79623cd835bdc471f9ca",
+    "url": "/static/css/main.31b2cbc0.chunk.css"
+  },
+  {
+    "revision": "a8f2a3a2d074c52989a5",
+    "url": "/static/js/2.9ebce455.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9ebce455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79623cd835bdc471f9ca",
+    "url": "/static/js/main.2bad7963.chunk.js"
+  },
+  {
+    "revision": "b10d08823a4df677ba67",
+    "url": "/static/js/runtime-main.de931e19.js"
+  }
+]);
